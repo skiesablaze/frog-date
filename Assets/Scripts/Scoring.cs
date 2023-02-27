835da15d74c9d7bf9d5f703c9c5010a63a06bfa0
therealzonder <zanon.mark@gmail.com>
2023-02-26 21:19:31 -0500

    Scoring done by variable names instead of hard coded names

--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-
-
 public class Scoring : MonoBehaviour
 {
-    private GetBones playerFrog;
-    private GetBones goalFrog;
+    private GetBones playerGetBones;
+    private GetBones goalGetBones;
     private List<Transform> goalBones;
     private List<Transform> playerBones;
+    public GameObject goalFrog;
     public Vector3 translation;
-    float score()
+    public string playerName;
+    public string goalName;
+
+
+    float score(string playerName, string goalName)
     {
         translation = new Vector3(0f,12f,0f);
         float pairdistance = 0;
         float totalDistance = 0;
 
-        playerFrog = GameObject.Find("frog2_manybones").GetComponent<GetBones>();
-        playerFrog.getBones();
-        playerBones = playerFrog.listOfBones;
+        playerGetBones = GameObject.Find(playerName).GetComponent<GetBones>();
+        playerGetBones.getBones();
+        playerBones = playerGetBones.listOfBones;
 
-        goalFrog = GameObject.Find("goalFrog").GetComponent<GetBones>();
-        goalFrog.getBones();
-        goalBones = goalFrog.listOfBones;
+        goalGetBones = GameObject.Find(goalName).GetComponent<GetBones>();
+        goalGetBones.getBones();
+        goalBones = goalGetBones.listOfBones;
         
         var playerAndGoal = playerBones.Zip(goalBones, (p, g) => new {player = p, goal = g});
         foreach(var bonePair in playerAndGoal)
@@ -32,22 +35,21 @@ public class Scoring : MonoBehaviour
             Vector3 worldgoal = transform.TransformPoint(bonePair.goal.position);
             Vector3 worldplayer = transform.TransformPoint(bonePair.player.position)+translation;
             pairdistance = Vector3.Distance(worldgoal, worldplayer);
-            totalDistance += pairdistance;
-            //totalDistance += Mathf.Pow(pairdistance, 2);
-            
+            totalDistance += pairdistance;            
         }
         totalDistance = 100 - 100 * totalDistance/playerAndGoal.Count();
         return(totalDistance);
     }
+    void Start() {
+        playerName = "frog2_manybones";
+        goalName = "cute_frog";
+    }
     void Update(){
-        if (Input.GetKeyDown("space")){
         float Score;
-        Score = score();
+        Score = score(playerName, goalName);
         if (Score > 99){
             Score = 100;
         }        
-        print("score = "+ Score);
         }
 
-    }
 }
