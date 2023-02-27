8efad4ae205ec43ce98ce5a567f1fa17b9daa059
therealzonder <zanon.mark@gmail.com>
2023-02-26 19:54:21 -0500

    added scoring and fixed infinite bones bug

--- Assets/Plugins/GetBones.cs
+++ Assets/Plugins/GetBones.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GetBones : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        foreach (Transform child in transform){
-            Debug.Log(child);
+    public List<Transform> listOfBones;
+    public bool testbool = true;
+
+    public void iterBones(Transform t){
+        listOfBones.Add(t.transform);
+        foreach(Transform child in t.transform){
+            iterBones(child);
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    public void getBones()
     {
-        
+        listOfBones = new List<Transform>();
+        iterBones(gameObject.transform);
+        foreach(var bone in listOfBones){
+        }
     }
 }
